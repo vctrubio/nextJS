b796e4aa160f038f9aa8904e31aaabad4002a17e
vctrubio <vctrubio@gmail.com>
2024-07-17 15:21:16 +0200

    globalcardstyleing

--- components/Card.jsx
+++ components/Card.jsx
@@ -2,24 +2,33 @@
 import React from "react";
 import { Ingredient } from "@prisma/client"
 import { DeleteBtn } from "@/components/Buttons";
+import { Category } from "@prisma/client";
 
 import SvgCart from "@/public/cart.svg"
 
+const rtnCatagoryColor = (category) => {
+
+    if (category.category === 'Fruit') {
+        return 'grey'
+    }
+    else
+        return 'purple'
+}
 
 export const Card = ({ ingredient }) => {
 
     return (
         <React.Fragment>
-            <div className="card-one" style={{ borderColor: 'red' }} >
+            <div className="card-one" style={{ border: `1px solid ${rtnCatagoryColor(ingredient)}` }} >
                 <div className="card-avatar">
                     <SvgCart width={50} height={50} />
                 </div>
                 <div style={{ backgroundColor: 'transparent', width: '100%' }}>
                     {ingredient.name}
+                    {/* , {ingredient.category} */}
                 </div>
             </div>
-            {/* {JSON.stringify(ingredient, null, 2)} */}
-            {/* <DeleteBtn ingredientId={ingredient.id} /> */}
+
         </React.Fragment>
     )
 }
@@ -36,6 +45,8 @@ export const CardSample = () => {
                     Apples
                 </div>
             </div>
+            {/* {JSON.stringify(ingredient, null, 2)} */}
+            {/* <DeleteBtn ingredientId={ingredient.id} /> */}
         </React.Fragment>
     )
 }
