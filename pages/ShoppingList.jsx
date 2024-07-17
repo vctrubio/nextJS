1aeff7591504872667cd6066c86b60d80538ac66
vctrubio <vctrubio@gmail.com>
2024-07-17 15:11:45 +0200

    localstorage

--- pages/ShoppingList.jsx
+++ pages/ShoppingList.jsx
@@ -3,24 +3,45 @@ import React, { useState, useEffect } from "react";
 import { getIngredientsDb } from "@/actions/queries";
 import { Card } from "@/components/Card";
 
+const LOCALSTORAGEPARAM = 'cart';
+
 const ShoppingListPage = () => {
-    const [ingredientBasket, setIngredientBasket] = useState([]);
+    const [ingredientBasket, setIngredientBasket] = useState({});
+    window.ig = ingredientBasket;
+
+    const loadLocalStorage = (ingredients) => {
+        let storedCart = JSON.parse(localStorage.getItem(LOCALSTORAGEPARAM)) || {};
+        let updatedBasket = { ...storedCart };
+
+        ingredients.forEach(ingredient => {
+            if (!storedCart[ingredient.id]) {
+                updatedBasket[ingredient.id] = { ...ingredient, cart: false };
+            }
+        });
+
+        setIngredientBasket(updatedBasket);
+        localStorage.setItem(LOCALSTORAGEPARAM, JSON.stringify(updatedBasket));
+    };
 
     useEffect(() => {
+        // localStorage.clear()
         getIngredientsDb().then((ingredients) => {
-            setIngredientBasket(ingredients.map(ingredient => ({ ...ingredient, cart: false })));
+            loadLocalStorage(ingredients);
         });
     }, []);
 
     const toggleCart = (id) => {
-        setIngredientBasket(prevBasket =>
-            prevBasket.map(ingredient =>
-                ingredient.id === id ? { ...ingredient, cart: !ingredient.cart } : ingredient
-            )
-        );
+        setIngredientBasket(prevBasket => {
+            const updatedBasket = {
+                ...prevBasket,
+                [id]: { ...prevBasket[id], cart: !prevBasket[id].cart }
+            };
+            localStorage.setItem(LOCALSTORAGEPARAM, JSON.stringify(updatedBasket));
+            return updatedBasket;
+        });
     };
 
-    const CardClickView = ({ ingredient}) => {
+    const CardClickView = ({ ingredient }) => {
         return (
             <div onClick={() => toggleCart(ingredient.id)}>
                 <Card ingredient={ingredient} />
@@ -28,36 +49,30 @@ const ShoppingListPage = () => {
         );
     };
 
-    /*
-    Cart is where ingredients are stored after being added to the cart (ingredient.cart == true)
-    Dispensary is where (ingredient.cart == false)
-    toggle between them while click, it should update the dom
-    */
-    return (<div className="shopping-list-container">
-        <div>
-            <h1>
-                Cart
-            </h1>
-            {ingredientBasket
-                .filter(ingredient => ingredient.cart)
-                .map((ingredient) => (
-                    <div key={ingredient.id}>
-                        <CardClickView ingredient={ingredient} />
-                    </div>))}
+    return (
+        <div className="shopping-list-container">
+            <div>
+                <h1>Cart</h1>
+                {Object.values(ingredientBasket)
+                    .filter(ingredient => ingredient.cart)
+                    .map((ingredient) => (
+                        <div key={ingredient.id}>
+                            <CardClickView ingredient={ingredient} />
+                        </div>
+                    ))}
+            </div>
+            <div>
+                <h1>Dispensary</h1>
+                {Object.values(ingredientBasket)
+                    .filter(ingredient => !ingredient.cart)
+                    .map((ingredient) => (
+                        <div key={ingredient.id}>
+                            <CardClickView ingredient={ingredient} />
+                        </div>
+                    ))}
+            </div>
         </div>
-        <div>
-            <h1 >
-                Dispensary
-            </h1>
-            {ingredientBasket
-                .filter(ingredient => !ingredient.cart)
-                .map((ingredient) => (
-                    <div key={ingredient.id}>
-                        <CardClickView ingredient={ingredient} />
-                    </div>
-                ))}
-        </div>
-    </div >);
+    );
 }
 
 export default ShoppingListPage;
