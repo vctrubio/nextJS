056d91045e5c68d9f4580f624b1db40208a97d76
vctrubio <vctrubio@gmail.com>
2024-07-20 21:24:47 +0200

    toast is working

--- actions/toast.js
+++ actions/toast.js
@@ -1,6 +1,39 @@
-import toast from 'react-hot-toast';
+"use client"
+import { Toaster, toast } from 'react-hot-toast';
+import { addIngredientDb } from '@/actions/queries';
+import { CategoryDropdown } from '@/components/Form';
 
 export const callToast = () => {
     toast.success('Ingredient added successfully!');
-    console.log('toast.sucess() called');
+}
+
+export const AddIngrediente = () => {
+    const addP = async (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const formData = new FormData(form);
+        const name = formData.get('name');
+        const category = formData.get('category');
+        const ingredient = { name, category };
+        if (await addIngredientDb(ingredient)) {
+            console.log('Ingredient added successfully.............!');
+        }
+        console.log('formData........................', formData.name);
+        callToast();
+        form.reset();
+    }
+
+    return (
+        <>
+            <form
+                style={{ color: 'red', gap: 5 }}
+                onSubmit={addP}
+            >
+                <input type="text" placeholder="name" required name='name' />
+                <CategoryDropdown />
+                <button>Add Ingredient</button>
+            </form>
+            <Toaster />
+        </>
+    )
 }
--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -4,7 +4,7 @@ import { AdminCardItem } from '@/components/Card';
 import { addIngredientDb } from '@/actions/queries';
 import { Category, Ingredient } from '@prisma/client';
 import { revalidatePath } from 'next/cache';
-import { callToast } from '@/actions/toast.js';
+import { AddIngrediente, callToast } from '@/actions/toast.js';
 
 const PAGE = '/admin';
 
@@ -21,33 +21,6 @@ const ViewAllIngredients = async () => {
         </>
     );
 };
-
-const AddIngrediente = async () => {
-    const addPost = async (formData: FormData) => {
-        "use server"
-        const name = formData.get('name');
-        const category = formData.get('category');
-        const ingredient = { name, category } as Ingredient;
-        console.log('hellooooooo')
-        if (await addIngredientDb(ingredient)) {
-            console.log('Ingredient added successfully!');
-            revalidatePath(PAGE);
-            //CALL TOAST not working becasue it is a server function calling a client function
-            //form.reset is not working too
-        }
-    }
-
-    return (
-        <form
-            style={{ color: 'red', gap: 5 }}
-            action={addPost}
-        >
-            <input type="text" placeholder="name" required name='name' />
-            <CategoryDropdown />
-            <button>Add Ingredient</button>
-        </form>
-    )
-}
 
 export default async function AdminPage() {
 
